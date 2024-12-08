9185992f0ceedfd2b96588b5aa30191ea6efae06
Denis BUGEJA <denis.bugeja@gmail.com>
2024-12-08 12:10:40 +0100

    WIP

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { Application, Controller } from "/stimulus.min.js"
-window.Stimulus = Application.start()
+// System import
 const { invoke } = window.__TAURI__.core
+
+// JS import
+import { Application, Controller } from "/stimulus.min.js"
+
+window.Stimulus = Application.start()
 
 Stimulus.register("budget", class extends Controller {
     static targets = ['textInput', 'message']
