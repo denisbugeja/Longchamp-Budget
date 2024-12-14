d65fce4b49ebf071ea7f8a5473b1632791a6be23
Denis BUGEJA <denis.bugeja@gmail.com>
2024-12-14 11:40:02 +0100

    Menus OK Boostrap

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri::menu::{MenuBuilder, MenuItemBuilder};
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,6 +10,19 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let quit = MenuItemBuilder::new("Quit").id("quit").build(app).unwrap();
+            let hide = MenuItemBuilder::new("Hide").id("hide").build(app).unwrap();
+            let show = MenuItemBuilder::new("Show").id("show").build(app).unwrap();
+            // we could opt handle an error case better than calling unwrap
+            let menu = MenuBuilder::new(app)
+                .items(&[&quit, &hide, &show])
+                .build()
+                .unwrap();
+
+            app.set_menu(menu).expect("Failed to set menu");
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
