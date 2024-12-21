533f24a225de7d462ef434239db96a152b3bef81
Denis BUGEJA <denis.bugeja@gmail.com>
2024-12-21 13:45:08 +0100

    WIP

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,7 +17,7 @@ fn greet(name: &str) -> String {
 fn update_db_path(path: &str) {
     let mut file_path = GLOBAL_FILE_PATH.write().unwrap();
     *file_path = String::from(path);
-    Connection::open(String::from(file_path.clone()));
+    Connection::open(String::from(file_path.clone())).expect("Unable to open file");
 
     println!("new File: {}", &file_path);
 }
