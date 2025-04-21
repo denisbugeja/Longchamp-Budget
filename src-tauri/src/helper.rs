79f7513d1bcb899ad3e51671df13bb896fa233ad
Denis BUGEJA <denis.bugeja@gmail.com>
2025-04-21 23:29:47 +0200

    WIP excel

--- src-tauri/src/helper.rs
+++ src-tauri/src/helper.rs
@@ -1,5 +1,5 @@
 use crate::repository;
-use rust_xlsxwriter::Workbook;
+use rust_xlsxwriter::{Format, FormatAlign, Formula, Workbook, Worksheet};
 use serde::{Deserialize, Serialize};
 use std::path::{Path, PathBuf};
 
@@ -79,6 +79,19 @@ pub fn json_to_vec(json_data: &str) -> Vec<&str> {
     serde_json::from_str(json_data).expect("Cannot deserialize section list")
 }
 
+fn encode_text(input: &str) -> String {
+    input
+        .chars()
+        .map(|c| {
+            if c.is_ascii_alphanumeric() || c == '_' {
+                c
+            } else {
+                '_'
+            }
+        })
+        .collect()
+}
+
 pub fn generate_xls_file() {
     let file_path = repository::get_global_file_path();
     let path = Path::new(&file_path);
@@ -87,12 +100,82 @@ pub fn generate_xls_file() {
     path_buf.set_extension("xlsx");
     let final_path = path_buf.to_string_lossy().into_owned();
 
+    let section_list: Vec<Section> = repository::section_list();
     let mut workbook = Workbook::new();
-    let worksheet = workbook.add_worksheet();
-    let mut row: u32 = 0;
-    let name = "test";
 
-    row += 1;
-    let _ = worksheet.write(row, 0, name);
+    for section in section_list {
+        handle_worksheet(&section, &mut workbook);
+    }
     let _ = workbook.save(final_path);
+}
+
+fn handle_worksheet(section: &Section, workbook: &mut Workbook) {
+    let worksheet: &mut Worksheet = workbook.add_worksheet();
+    let title_format = Format::new().set_bold().set_align(FormatAlign::Center);
+    let calculated_expenses_list: Vec<CalculatedExpense> =
+        repository::get_calculated_expenses(&section.uid);
+    let mut row: u32 = 1;
+
+    let _ = worksheet
+        .set_name(encode_text(&section.title))
+        .expect("Impossible to set the sheet's name");
+
+    let _ = worksheet.merge_range(0, 0, 0, 6, &section.title, &title_format);
+
+    let _ = worksheet.write(row, 0, "Enfants/Ados:");
+    let _ = worksheet.write(row, 1, section.members_count);
+    row += 1;
+
+    let _ = worksheet.write(row, 0, "Chefs:");
+    let _ = worksheet.write(row, 1, section.adults_count);
+
+    row += 3;
+    let _ = worksheet.write(row, 0, "Libellé");
+    let _ = worksheet.write(row, 1, "Prix unitaire");
+    let _ = worksheet.write(row, 2, "Enfants/Ados");
+    let _ = worksheet.write(row, 3, "Chefs");
+    let _ = worksheet.write(row, 4, "%");
+    let _ = worksheet.write(row, 5, "Commentaires");
+    let _ = worksheet.write(row, 6, "Total");
+    row += 1;
+
+    for expense in calculated_expenses_list {
+        let unit_price = match expense.expenses_instances_unit_price {
+            Some(val) => val,
+            None => expense.expenses_unit_price.unwrap(),
+        };
+        let children = match expense.expenses_instances_units {
+            Some(val) => val,
+            None => expense.expenses_units.unwrap(),
+        };
+        let adults = match expense.expenses_instances_units_adults {
+            Some(val) => val,
+            None => expense.expenses_units_adults.unwrap(),
+        };
+        let rate = match expense.expenses_instances_rate {
+            Some(val) => val,
+            None => expense.expenses_rate.unwrap(),
+        };
+
+        let formula_row = row + 1;
+        let formula_string = format!(
+            "=B{}*(C{}+D{})*(E{}/100)",
+            formula_row, formula_row, formula_row, formula_row
+        );
+
+        let result: f32 = expense.total_applyed_price.unwrap();
+        let formula = Formula::new(formula_string.as_str()).set_result(result.to_string());
+
+        let _ = worksheet.write(row, 0, expense.title_expense.clone());
+        let _ = worksheet.write(row, 1, unit_price);
+        let _ = worksheet.write(row, 2, children);
+        let _ = worksheet.write(row, 3, adults);
+        let _ = worksheet.write(row, 4, rate);
+        let _ = worksheet.write(row, 5, expense.comments.clone());
+        let _ = worksheet.write_formula(row, 6, formula);
+
+        row += 1
+    }
+
+    let _ = worksheet.autofit();
 }
