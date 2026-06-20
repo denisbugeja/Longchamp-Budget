29ca42e024e85d0e84b261b17696e771b07273e4
Denis BUGEJA <denis.bugeja@gmail.com>
2026-06-20 14:23:01 +0200

    wording: modification message

--- src/main.js
+++ src/main.js
@@ -1434,7 +1434,7 @@ Stimulus.register("matrix-expense-instance", class extends Controller {
      */
     async deleteExpenseInstance() {
         this.element.classList.add('table-active')
-		if (await confirm("Êtes vous sûr de vouloir supprimer cette dépense ?")) {
+		if (await confirm("Êtes-vous sûr(e) de vouloir supprimer cette dépense ?")) {
             await invoke("delete_expense_instance", { uidExpenseInstance: this.uidValue })
             this.matrixSectionOutlet.triggerGlobalRefresh()
         }
